61e2348278134b23dd76d91e11dc793de7fab0bb
Killuazldyck <abil53682@gmail.com>
2025-11-06 13:00:44 +0700

    web

---
+++ main.js
@@ -0,0 +1,8 @@
+nama = prompt("Nama anda: ")
+usia = prompt("Usia anda: ")
+if (usia >= 50) {
+  alert("usia anda terlalu tua")
+} else {
+  alert(`nama anda adalah ${nama} dan usia anda adalah ${usia}`)
+}
+
